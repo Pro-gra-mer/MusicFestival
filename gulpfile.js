09b6f0217aeccefb24cb8c2e958c0d817f7dceb0
Rebeca <rbk_po@hotmail.com>
2024-03-22 18:38:54 +0100

    Comprimido

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,24 @@ const { src, dest, watch, parallel } = require ("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
 
+// Para comprimir css
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Para comprimir js
+const terser = require('gulp-terser-js');
+
+
 function css (cb) {
     
     src('src/scss/**/*.scss')// Identificar el archivo de sass
+    .pipe(sourcemaps.init())
     .pipe( plumber())// Evita detención de la ejecución ante algún error
     .pipe( sass() )// Compilarlo
+    .pipe(postcss([ autoprefixer(), cssnano() ]))
+    .pipe(sourcemaps.write('.'))
     .pipe( dest("build/css")) // Guardarlo
     
     cb(); // Callback que avisa a gulp cuando llegamos al final
@@ -14,6 +27,9 @@ function css (cb) {
 
 function javascript(cb) {
     src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     cb();
