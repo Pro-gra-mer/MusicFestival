d1616897c386d73f1de5745ebc45a7d960820ace
Rebeca <rbk_po@hotmail.com>
2024-03-22 17:46:42 +0100

    Finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,38 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function iciarApp() {
+    navegacionFija();
     crearGaleria();
+    scrollNav();
 };
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+
+    window.addEventListener('scroll', function() {
+
+        if (sobreFestival.getBoundingClientRect().bottom < 0) {
+            barra.classList.add('fijo');
+        } else {
+            barra.classList.remove('fijo');
+        }
+    })
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth" })
+        })
+    })
+}
 
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
@@ -53,3 +83,5 @@ function mostrarImagen(id) {
         // Añade clase que bloquea el scroll
         body.classList.add('fijar-body');   
     }
+
+    
